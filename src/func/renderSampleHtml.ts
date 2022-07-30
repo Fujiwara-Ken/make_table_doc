477172306dcc6d2d3fdd1c664f9780ad125262c8
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-07-30 16:10:04 +0900

    render html table def add

--- src/func/renderSampleHtml.ts
+++ src/func/renderSampleHtml.ts
@@ -7,7 +7,7 @@ import path from 'path';
 import fs from 'fs';
 
 const templatePath = './src/template/table_def.ejs';
-const dataPath = './src/data/data-sample.json';
+const dataPath = './src/data/sample-table-list.json';
 const outputPath = './out/';
 
 const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
@@ -15,26 +15,12 @@ const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
 console.log(data);
 
 const render = async () => {
-  // const data = { title: 'ページタイトル', content: 'テスト', foot: 'フッター' };
-  // await ejs.renderFile(templatePath, data, function (err, html) {
-  //   console.log(html);
-
-  //   fs.writeFile(outputPath, html, 'utf8', (err: any) => {
-  //     if (err) {
-  //       console.log(err);
-  //     } else {
-  //       console.log('保存完了');
-  //     }
-  //   });
-  // });
-  data.forEach((item: ejs.Data) => {
-    ejs.renderFile(templatePath, item, (error, output) => {
-      if (error) {
-        console.log(error);
-      } else {
-        fs.writeFileSync(path.join(outputPath, `tabe_def.html`), output);
-      }
-    });
+  await ejs.renderFile(templatePath, { data }, (error, output) => {
+    if (error) {
+      console.log(error);
+    } else {
+      fs.writeFileSync(path.join(outputPath, `tabe_def.html`), output);
+    }
   });
 };
 
